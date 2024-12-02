a1e9f80ac26a1e3c9ee788a081b349556e655128
Cansu Altun <cansualtun@Cansu-MacBook-Air.local>
2024-12-02 13:58:02 +0300

    fix

--- src/app/[locale]/components/contactForm.tsx
+++ src/app/[locale]/components/contactForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from 'react';
+import { useState,useRef } from 'react';
 import { motion } from 'framer-motion';
 import emailjs from '@emailjs/browser';
 import { Send, Loader2 } from 'lucide-react';
